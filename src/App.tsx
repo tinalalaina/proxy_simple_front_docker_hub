7447c6fc4aa5b5f81bfc308ebca4ab20115eb8c8
TL Raky <120471799+tinalalaina@users.noreply.github.com>
2026-04-03 15:37:26 +0300

    Clean up App.tsx by removing images and text
    Removed unused images and introductory text.

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,11 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          
         </div>
         <div>
           <h1>Get started whith docker front only and vps</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          
         </div>
         <button
           className="counter"
